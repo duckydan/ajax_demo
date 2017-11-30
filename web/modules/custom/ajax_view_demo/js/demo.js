e8a4c7e4a53bfef0363a656279e51d74abc27fb5
Dan Eveland <develand@prometsource.com>
2017-11-30 13:32:57 -0500

    Simplified the JS

--- web/modules/custom/ajax_view_demo/js/demo.js
+++ web/modules/custom/ajax_view_demo/js/demo.js
@@ -1,128 +1,48 @@
 (function ($, Drupal, drupalSettings) {
-  Drupal.behaviors.ajaxViewDemoToday = {
-    attach: function (context, settings) {
-      // Attach ajax action click event of each view pager.
-      $('.calendar-today').once('attach-links').each(this.attachLink);
-   },
 
-    attachLink: function (idx, pager) {
-
-      // This is where the actual date calculations will go
-      sessionStorage.setItem('date', 201711);
-      var date = sessionStorage.getItem('date');
-      var view_args = date;
-      
-      // Everything we need to specify about the view.
-      var view_info = {
-        view_name: 'calendar',
-        view_display_id: 'embed_1',
-        view_args: view_args,
-        view_dom_id: 'ajax-demo'
-      };
-
-      // Details of the ajax action.
-      var ajax_settings = {
-        submit: view_info,
-        url: '/views/ajax',
-        element: pager,
-        event: 'click'
-      };
-      
-      alert( "This calendar month." );
-
-
-      Drupal.ajax(ajax_settings);
-    }
+    var element = document.getElementById('calendar-today');
     
-  };
+    element.onclick = function() {
+        // This is where the actual date calculations will go
+        console.log(this);
+        // sessionStorage.setItem('date', '201710');
+        var startdate = parseInt(sessionStorage.getItem('date'));
+        var increment = 500;
+        var date = startdate - increment;
+        var view_args = date;
+        sessionStorage.setItem('date', date.toString());
+
+        console.log(view_args)
+        // Everything we need to specify about the view.
+        var view_info = {
+          view_name: 'calendar',
+          view_display_id: 'embed_1',
+          view_args: view_args,
+          view_dom_id: 'ajax-demo'
+        };
+
+        // Details of the ajax action.
+        var ajax_settings = {
+          submit: view_info,
+          url: '/views/ajax',
+          element: this,
+          event: 'click'
+        };
+
+        Drupal.ajax(ajax_settings);
+        alert( "This calendar month." );
+        alert(sessionStorage.getItem('date'));
+
+   }
+
 })(jQuery, Drupal, drupalSettings);
 
-(function ($, Drupal, drupalSettings) {
-  Drupal.behaviors.ajaxViewDemoPrev = {
-    attach: function (context, settings) {
-      // Attach ajax action click event of each view pager.
-      $('.calendar-prev').once('attach-links').each(this.attachLink);
-   },
 
-    attachLink: function (idx, pager) {
-
-      // This is where the actual date calculations will go
-      var startdate = parseInt(sessionStorage.getItem('date'));
-      var increment = 100;
-      var date = startdate - increment;
-      var view_args = date;
-      sessionStorage.setItem('date', date.toString());
-      
-      // Everything we need to specify about the view.
-      var view_info = {
-        view_name: 'calendar',
-        view_display_id: 'embed_1',
-        view_args: view_args,
-        view_dom_id: 'ajax-demo'
-      };
-
-      // Details of the ajax action.
-      var ajax_settings = {
-        submit: view_info,
-        url: '/views/ajax',
-        element: pager,
-        event: 'click'
-      };
-
-      alert( "Previous calendar month." );
-
-      Drupal.ajax(ajax_settings);
-    }
-    
-  };
-})(jQuery, Drupal, drupalSettings);
-
-(function ($, Drupal, drupalSettings) {
-  Drupal.behaviors.ajaxViewDemoNext = {
-    attach: function (context, settings) {
-      // Attach ajax action click event of each view pager.
-      $('.calendar-next').once('attach-links').each(this.attachLink);
-   },
-
-    attachLink: function (idx, pager) {
-
-      // This is where the actual date calculations will go
-      var startdate = parseInt(sessionStorage.getItem('date'));
-      var increment = 500;
-      var date = startdate + increment;
-      var view_args = date;
-      var results = sessionStorage.setItem('date', date.toString());
-      $('body').append(results);
-
-      // Everything we need to specify about the view.
-      var view_info = {
-        view_name: 'calendar',
-        view_display_id: 'embed_1',
-        view_args: view_args,
-        view_dom_id: 'ajax-demo'
-      };
-
-      // Details of the ajax action.
-      var ajax_settings = {
-        submit: view_info,
-        url: '/views/ajax',
-        element: pager,
-        event: 'click'
-      };
-
-      alert( "Next calendar month." );
-
-      Drupal.ajax(ajax_settings);
-    }
-    
-  };
-})(jQuery, Drupal, drupalSettings);
-
-jQuery(function ($) {
-  $('body').append('<p>Hello World! ');
-  var date = sessionStorage.getItem('date');
-  $('body').append(date);
-  $('body').append('</p>');
-  $( ".calendar-today" ).css( "border", "6px solid red" );
-  $( ".calendar-today" ).trigger('click');
-});
+// jQuery(function ($) {
+//   sessionStorage.setItem('date', '201711');
+//   $( ".calendar-today" ).css( "border", "6px solid red" );
+//   $( ".calendar-today" ).trigger('click');
+//   $( ".calendar-today" ).trigger('click');
+//  alert('the starting value is: ' + sessionStorage.getItem('date'));
+// 
+// });
